ecd8bb27385856aefa0e380278ab3ef7a59ec82e
Simon Michael <simon@joyful.com>
2022-05-14 08:56:25 -1000

    web: htmx.js, disabled

--- web/index.php
+++ web/index.php
@@ -129,6 +129,7 @@ td.tags {
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
   <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
   <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+  <!-- <script src="js/htmx.min.js" defer></script> -->
   <script>
 
 // https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
