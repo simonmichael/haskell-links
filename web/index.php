72f087ebb95a13b432061eeda0e426c2ae80bd06
Simon Michael <simon@joyful.com>
2022-05-20 23:09:24 -1000

    web: fix twitter link layout

--- web/index.php
+++ web/index.php
@@ -166,8 +166,8 @@ function index() {
       <a href="https://www.haskell.org/irc/">IRC chat</a> |
       <a href="https://view.matrix.org/?query=haskell">Matrix chat</a> |
       <a href="https://stackoverflow.com/questions/tagged/haskell">Stack Overflow</a> |
-      <a href="https://haskell.foundation/podcast">HF podcast</a>
-      <a href="https://twitter.com/HaskellOrg">Twitter</a> |
+      <a href="https://haskell.foundation/podcast">HF podcast</a> |
+      <a href="https://twitter.com/HaskellOrg">Twitter</a>
       <br>
       &nbsp;
       <a href="https://cabal.readthedocs.io">Cabal user guide</a> |
